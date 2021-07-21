7ec259240c38f7890f7e174c0072608571751119
James Futhey <kidGodzilla@users.noreply.github.com>
2021-07-20 22:54:02 -0700

    Use pushstate instead of hashchange for routing

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
       <b-navbar toggleable="lg" type="dark" variant="primary">
 
         <div class="container">
-          <b-navbar-brand href="#/hello">Starter App</b-navbar-brand>
+          <b-navbar-brand href="/hello">Starter App</b-navbar-brand>
 
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
           <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-nav-item href="#/location">Location</b-nav-item>
+              <b-nav-item href="/location">Location</b-nav-item>
             </b-navbar-nav>
           </b-collapse>
 
