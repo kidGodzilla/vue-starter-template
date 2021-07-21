dec348191ac0b46d3d94447f8bdcef11be465403
James Futhey <kidGodzilla@users.noreply.github.com>
2021-07-21 13:29:34 -0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,7 @@
     <h3 class="mb-2">Ecosystem</h3>
     <ul>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener"><del>vuex</del></a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"><del>vue-loader</del></a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
@@ -39,8 +39,6 @@ export default {
   name: 'HelloWorld',
   props: {
     //msg: String
-  },
-  methods: {
   },
   data() {
     return {
